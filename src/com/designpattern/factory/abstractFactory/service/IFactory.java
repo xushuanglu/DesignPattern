cf432c3a1e2f5fc2ebacb01ec9fe9747ee4043f8
xushuanglu <xushuanglu_java@163.com>
2019-02-27 10:45:08 +0800

    test

--- src/com/designpattern/factory/abstractFactory/service/IFactory.java
+++ src/com/designpattern/factory/abstractFactory/service/IFactory.java
@@ -1,7 +1,9 @@
 package com.designpattern.factory.abstractFactory.service;
 
-public interface SqlFactory {
+public interface IFactory {
 
 	public IUser createUser(); // 用于访问User表的对象
 
+    public ILogin createLogin();
+    
 }
