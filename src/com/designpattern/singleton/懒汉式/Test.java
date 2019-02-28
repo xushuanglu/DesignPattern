d8b65d162ef2d7cd7dbfa20fc573495fe2beab63
xushuanglu <xushuanglu_java@163.com>
2019-02-28 14:57:10 +0800

    singleton单例模式代码示例添加

---
+++ src/com/designpattern/singleton/懒汉式/Test.java
@@ -0,0 +1,25 @@
+package com.designpattern.singleton.懒汉式;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Thread[] threads = new Thread[10];
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new TestThread();
+		}
+
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
+	}
+
+}
+
+	class TestThread extends Thread {
+		@Override
+		public void run() {
+			// 对于不同单例模式的实现，只需更改相应的单例类名及其公有静态工厂方法名即可
+			int hash = LHanDanli.getSingleton().hashCode();
+			System.out.println(hash);
+		}
+	}
